fa86da79e3e4c7f485c9741fb5687b916ed56f06
Mona Lisa <benlevy01@gmail.com>
2026-03-26 20:21:50 +0200

    yea

--- scripts/docker_install.sh
+++ scripts/docker_install.sh
@@ -17,6 +17,6 @@ EOF
 
 sudo apt update
 # Docker packages installation
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker $USER
 
